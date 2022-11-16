d50ef2e3a0e544cc3789e04b47d44198a3e10f24
SANGWOORYU <rookas33@gmail.com>
2022-11-16 16:08:29 +0900

    Update Comment.js

--- FourGlass/static/js/Comment.js
+++ FourGlass/static/js/Comment.js
@@ -1,80 +1,75 @@
-function isDefined(value){
-    if(value === "" || value === undefined || value === null){ //value가 비정상적인 값이면 false를 반환
-        return false
-    }
-    return true
+src="https://code.jquery.com/jquery-3.4.1.js"
+$(document).ready(function () {
+    set_temp()
+    show_comment()
+});
+
+function set_temp() {
+    $.ajax({
+        type: "GET",
+        url: "/main",
+        data: {},
+        success: function (response) {
+            $('#temp').text(response['temp'])
+        }
+    })
+
+
 }
 
-function comment(get_url, add_url){ //리스트 가져오기 위한 get_url, 추가할 add_url
-    if($('.name').val()=="" || $('.comment').val()=="" || $('#pass').val()==""){ //유효성 검사
-        alert('이름이나 내용을 입력해주세요.');
-        return;
-    }
-    let idx
-    function create_index(callbackfunc){ //db의 comment리스트 가져오기
-        $.ajax({
-            type : "GET",
-            url : get_url,
-            data : {},
-            success : function(response){
-                let rows = response['comments']
+function save_maincomment() {
+    let name = $('#name').val()
+    let comment = $('#comment').val()
 
-                callbackfunc(rows)
-            },
-            error : function(response) {
-                console.log(response)
-            }
-        });
+    if ($("#name").val().length == 0) {
+        alert("이름을 입력해주세요!");
+        $("#name").focus();
+        return false;
     }
-    create_index(function(rows){ //콜백함수 create_index가 성공해야 실행됨
-        console.log(rows)
-        isDefined(rows[0]['idx']) ? idx=0 : idx=1 //db에 아무것도 없을 경우 idx가 undefined가 되어 1로 지정
-        for(let i=0; i<rows.length; i++) {
-            let temp = rows[i]['idx']
-            if(temp>idx) {idx=temp}
+
+    if ($("#comment").val().length == 0) {
+        alert("댓글작성해주세요~");
+        $("#comment").focus();
+        return false;
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/main',
+        data: {name_give: name, comment_give: comment},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
         }
-        idx++
-        let name = $('#name').val();
-        let comment = $('#comment').val();
-        let pass = $('.pass').val();
-        $.ajax({
-            type : 'POST',
-            url : add_url,          //클릭한 버튼의 url
-            data : {'idx_give':idx, 'name_give':name, 'comment_give':comment, 'pass_give':pass},
-            success: function(response){
-                alert("추가되었습니다.")
-                location.reload()
-            },
-            error : function(response){
-                console.log(response);
-            }
-        });
     })
 }
 
-function show_comment(show_url){
-    $('#sampleDiv').empty()
+function show_comment() {
     $.ajax({
-        type : 'get',
-        url : show_url,
-        data : {},
-        success : function (response){
-            let rows = response['comments']
-            for(let i=0; i<rows.length; i++){
-                let idx = rows[i]['idx']
+        type: "GET",
+        url: "/main",
+        data: {},
+        success: function (response) {
+            let rows = response['main']
+            for (let i = 0; i < rows.length; i++) {
                 let name = rows[i]['name']
                 let comment = rows[i]['comment']
-                let temp_div = `<div style="overflow: hidden" id="replyCmt">
-                                    <p style="float:left; margin:0 3px; padding:10px; border: 1px;">${name}</p>
-                                    <p style="float:left; margin:0 3px; padding:10px; border: 1px;">${comment}</p>
-                                    <button style="float:left; class="`+ idx +`"margin-top:7px" id="openReply" type="button" class="\` + idx + \`">↓</button>
-                                    <button class="${rows[i]['idx']}" id="replyBtn" type="button" style="float:left; margin:0 3px; padding:10px; border: 1px;">댓글</button>
-                                    <button class="${rows[i]['idx']}" id="delBtn" type="button" style="float:right; margin:0 3px; padding:10px; border: 1px;">삭제</button>
-                                </div>
-                                <div id="replyModal" class="modal`+ idx +`" style="display:none">댓글이 보여지는 곳</div>`
-                $('#sampleDiv').append(temp_div)
+
+
+                let temp_html = `<div class="card" >
+                                            <div class="card-body" style="box-shadow: black">
+                                                <blockquote class="blockquote mb-0">
+                                                    <p><span style="color: #7140dd">닉네임:</span> ${name}</p>
+                                                    <footer onclick="done_main()" class="blockquote-footer">${comment}</footer>
+                                                </blockquote>
+                                            </div>
+                                        </div>`
+                $('#comment-list').append(temp_html)
+
+
             }
         }
-    })
+    });
 }
+
 
