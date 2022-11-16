81e0be29f5386a4e1fb198dcbfdef5d16168aeec
JiHoon Song <jihoonsong@JiHoonui-MacBookPro.local>
2022-11-16 12:08:32 +0900

    송지훈연결

--- FourGlass/app.py
+++ FourGlass/app.py
@@ -1,3 +1,4 @@
+from pymongo import MongoClient
 import certifi
 
 from flask import Flask, render_template, request, jsonify
@@ -5,23 +6,78 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 ca = certifi.where()
-from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://test:sparta@cluster0.uerebxa.mongodb.net/?retryWrites=true&w=majority",
-                     tlsCAFile=ca)
+client = MongoClient(
+    "mongodb+srv://test:sparta@cluster0.uerebxa.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
 db = client.sparta
 
+# 팀소개 페이지
+
+
+# ------------------메인페이지 댓글----------------
 @app.route('/')
 def home():
     return render_template('mainpage.html')
 
+# 송지훈 개인페이지
+
+
+@app.route('/jh')
+def jh():
+    return render_template('jh.html')
+
+# 송지훈 개인페이지
+
+
+@app.route("/jh/review", methods=["POST"])
+def insertReviewPost():
+    review_receive = request.form['review_give']
+
+    doc = {
+        'review': review_receive,
+    }
+    db.jh.insert_one(doc)
+    return jsonify({'msg': '저장완료'})
+
+# 송지훈 개인페이지
+
+
+@app.route("/jh/review", methods=["GET"])
+def homework_get_jh():
+    reviews = list(db.jh.find({}, {'_id': False}).sort('_id', -1).limit(3))
+    print(reviews)
+    return jsonify({'reviews': reviews})
+
+@app.route("/", methods=["POST"])
+def intro_maindet():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    maindet_list = list(db.imaindet.find({}, {'_id': False}))
+    count = len(maindet_list) + 1
+
+    doc = {'num': count, 'name': name_receive, 'comment': comment_receive}
+    db.maindet.insert_one(doc)
+
+    return jsonify({'msg': '댓글감사합니다!!'})
+
+
+@app.route("/maindet", methods=["GET"])
+def maindet_get():
+    maindet_list = list(db.maindet.find({}, {'_id': False}))
+    return jsonify({'intro': maindet_list})
+
+
+# ----------------------------------------------
 @app.route('/team1')
 def team1():
     return render_template('team1.html')
+
+
 # ------------------team3yook---------------------------------
 @app.route('/team3')
 def team3():
     return render_template('team3.html')
+
 
 # <<<<<<< HEAD
 @app.route("/intro", methods=["POST"])
@@ -31,17 +87,17 @@ def intro_post():
     intro_list = list(db.intro.find({}, {'_id': False}))
     count = len(intro_list) + 1
 
-    doc = {'num' : count , 'name': name_receive , 'comment': comment_receive}
+    doc = {'num': count, 'name': name_receive, 'comment': comment_receive}
     db.intro.insert_one(doc)
 
-    return jsonify({'msg':'응원 감사합니다!!'})
-
+    return jsonify({'msg': '응원 감사합니다!!'})
 
 
 @app.route("/intro", methods=["GET"])
 def intro_get():
     intro_list = list(db.intro.find({}, {'_id': False}))
-    return jsonify({'intro':intro_list})
+    return jsonify({'intro': intro_list})
+
 
 # ------------------team3yook---------------------------------
 
@@ -68,23 +124,27 @@ def homework_get():
     comment_list = list(db.comment.find({}, {'_id': False}))
     return jsonify({'comments': comment_list})
 
+
 @app.route("/fourglass/findReply", methods=["POST"])
 def fourglass_findReply():
     idx_receive = request.form["id_give"]
-    find_list = list(db.reply.find({"idx":idx_receive}))
+    find_list = list(db.reply.find({"idx": idx_receive}))
     return jsonify({'result': find_list})
+
 
 @app.route("/fourglass/find", methods=["POST"])
 def homework_find():
     id_receive = request.form["id_give"]
-    find_list = list(db.comment.find({"name":id_receive}, {'_id': False}))
+    find_list = list(db.comment.find({"name": id_receive}, {'_id': False}))
     return jsonify({'result': find_list})
+
 
 @app.route("/fourglass/del", methods=["POST"])
 def homework_del():
     id_receive = request.form["id_give"]
     db.comment.delete_one({'name': id_receive})
     return jsonify({'msg': '삭제 완료!'})
+
 
 @app.route("/fourglass/addReply", methods=["POST"])
 def fourglass_addReply():
@@ -100,9 +160,11 @@ def fourglass_addReply():
     db.reply.insert_one(doc)
     return render_template('team1.html')
 
+
 @app.route('/team2')
 def team2():
     return render_template('team2.html')
+
 
 @app.route("/fourglass/team2/addReply", methods=["POST"])
 def teamTwo_post():
@@ -122,6 +184,6 @@ def teamTwo_get():
     comment_list = list(db.teamTwo.find({}, {'_id': False}))
     return jsonify({'comments': comment_list})
 
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
