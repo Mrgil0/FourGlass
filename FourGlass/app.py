9946db5f51c57e6651ddee92391d70e3d46cac31
Mrgil0 <dieoxin4@naver.com>
2022-11-16 10:04:57 +0900

    컨트롤러 수정

--- FourGlass/app.py
+++ FourGlass/app.py
@@ -10,6 +10,11 @@ client = MongoClient("mongodb+srv://test:sparta@cluster0.uerebxa.mongodb.net/?re
                      tlsCAFile=ca)
 db = client.sparta
 
+
+@app.route('/')
+def home():
+    return render_template('team1.html')
+
 @app.route("/homework", methods=["POST"])
 def homework_post():
     idx_receive = request.form["idx_give"]
@@ -65,3 +70,4 @@ def fourglass_addReply():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
