d57a7efbd456d1e7a15e85346cce3638872d6761
six-ju <82314404+six-ju@users.noreply.github.com>
2022-11-16 09:43:12 +0900

    team3(2)
    app.py 기능 추가

--- FourGlass/app.py
+++ FourGlass/app.py
@@ -15,7 +15,35 @@ db = client.sparta
 @app.route('/')
 def home():
     return render_template('team1.html')
+# ------------------team3yook---------------------------------
+@app.route('/')
+def home():
+    return render_template('team3.html')
 
+@app.route("/intro", methods=["POST"])
+def intro_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    intro_list = list(db.intro.find({}, {'_id': False}))
+    count = len(intro_list) + 1
+
+    doc = {'num' : count , 'name': name_receive , 'comment': comment_receive}
+    db.intro.insert_one(doc)
+
+    return jsonify({'msg':'응원 감사합니다!!'})
+
+
+
+@app.route("/intro", methods=["GET"])
+def intro_get():
+    intro_list = list(db.intro.find({}, {'_id': False}))
+    return jsonify({'intro':intro_list})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
+
+# ------------------team3yook---------------------------------
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
