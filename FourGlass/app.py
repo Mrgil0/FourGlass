438dc279dc666134a5988b6ddb9ef8b415b3e136
SANGWOORYU <rookas33@gmail.com>
2022-11-16 10:18:49 +0900

    team2페이지 댓글기능추가

--- FourGlass/app.py
+++ FourGlass/app.py
@@ -68,6 +68,24 @@ def fourglass_addReply():
     db.reply.insert_one(doc)
     return render_template('team1.html')
 
+@app.route("/4glass", methods=["POST"])
+def teamTwo_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+    db.teamTwo.insert_one(doc)
+
+    return jsonify({'msg': '작성 완료!'})
+
+
+@app.route("/4glass", methods=["GET"])
+def teamTwo_get():
+    comment_list = list(db.teamTwo.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
